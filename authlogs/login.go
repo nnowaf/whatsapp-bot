16a5c19ed586589a8479a2cb8d716a0fc7bf5828
Mohammad Nowaf <muhammadnowaf2608@gmail.com>
2022-10-30 04:24:44 +0700

    add trigger chat

--- authlogs/login.go
+++ authlogs/login.go
@@ -5,25 +5,83 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"github.com/golang/protobuf/proto"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mdp/qrterminal"
 	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
-func eventHandler(evt interface{}) {
+var client *whatsmeow.Client
+
+func myEventHandler(evt interface{}) {
 	switch v := evt.(type) {
 	case *events.Message:
-		fmt.Println("Received a message!==============", v.Message.GetConversation())
-		fmt.Println(v.Message.GetConversation())
+		fmt.Println("GetConversation : ", v.Message.GetConversation())
+		fmt.Println("Sender : ", v.Info.Sender)
+		fmt.Println("Sender Number : ", v.Info.Sender.User)
+		fmt.Println("IsGroup : ", v.Info.IsGroup)
+		fmt.Println("MessageSource : ", v.Info.MessageSource)
+		fmt.Println("ID : ", v.Info.ID)
+		fmt.Println("PushName : ", v.Info.PushName)
+		fmt.Println("BroadcastListOwner : ", v.Info.BroadcastListOwner)
+		fmt.Println("Category : ", v.Info.Category)
+		fmt.Println("Chat1 : ", v.Info.Chat)
+		fmt.Println("Chat2 Server : ", v.Info.Chat.Server)
+		fmt.Println("Chat3 User : ", v.Info.Chat.User)
+		fmt.Println("DeviceSentMeta : ", v.Info.DeviceSentMeta)
+		fmt.Println("IsFromMe : ", v.Info.IsFromMe)
+		fmt.Println("MediaType : ", v.Info.MediaType)
+		fmt.Println("Multicast : ", v.Info.Multicast)
+		fmt.Println("Info.Chat.Server : ", v.Info.Chat.Server)
+		// fmt.Println("livelocation : ", v.Message.LiveLocationMessage.DegreesLatitude)
+		msg := &waProto.Message{
+			Conversation: proto.String("Hello World WJAIODHWAIDHWAHDUIWAHDUIWAHDUIWAHDUIWA\nDWUAHDUIWAHDUIAW\nUIDHWAUIDAWH"),
+		}
+		sendTo, ok := parseJID(v.Info.Chat.User)
+		if !ok {
+			return
+		}
+
+		if v.Message.GetConversation() == "nowaf" {
+			respon, err := client.SendMessage(context.Background(), sendTo, "", msg)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			fmt.Println(respon)
+		}
+	case *events.Receipt:
+
 	}
 }
 
+func parseJID(arg string) (types.JID, bool) {
+	if arg[0] == '+' {
+		arg = arg[1:]
+	}
+	if !strings.ContainsRune(arg, '@') {
+		return types.NewJID(arg, types.DefaultUserServer), true
+	} else {
+		recipient, err := types.ParseJID(arg)
+		if err != nil {
+			log.Errorf("Invalid JID %s: %v", arg, err)
+			return recipient, false
+		} else if recipient.User == "" {
+			log.Errorf("Invalid JID %s: no server specified", arg)
+			return recipient, false
+		}
+		return recipient, true
+	}
+}
 func LoginAuth() {
+
 	dbLog := waLog.Stdout("Database", "DEBUG", true)
 	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
 	container, err := sqlstore.New("sqlite3", "file:wapp.db?_foreign_keys=on", dbLog)
@@ -36,9 +94,8 @@ func LoginAuth() {
 		panic(err)
 	}
 	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	fmt.Println(clientLog)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
-	client.AddEventHandler(eventHandler)
+	client = whatsmeow.NewClient(deviceStore, clientLog)
+	client.AddEventHandler(myEventHandler)
 
 	if client.Store.ID == nil {
 		// No ID stored, new login
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	filippo.io/edwards25519 v1.0.0 // indirect
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	go.mau.fi/libsignal v0.0.0-20221015105917-d970e7c3c9cf // indirect
 	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a // indirect
@@ -19,6 +20,8 @@ require (
 )
 
 require (
+	github.com/mdp/qrterminal/v3 v3.0.0
 	go.mau.fi/whatsmeow v0.0.0-20221025074022-0f246f12636b
+	google.golang.org/appengine v1.6.7
 	rsc.io/qr v0.2.0 // indirect
 )
